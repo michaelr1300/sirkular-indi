f275bade9ffce73403c0db260fd53ea1ef075dd5
Michael Roberto <michaelr1300@gmail.com>
2022-03-20 19:32:49 +0700

    Models & Relation

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+
+    public function order()
+    {
+        return $this->belongsTo('App\Models\Order');
+    }
+
+    public function package()
+    {
+        return $this->hasOne('App\Models\Package');
+    }
+}
