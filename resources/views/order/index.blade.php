4c7f3c03637c398b7c7448b02b1f80551f89c597
Michael Roberto <michaelr1300@gmail.com>
2022-03-20 19:34:29 +0700

    Order Controller & Routing

---
+++ resources/views/order/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    Order List
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,4 +30,13 @@ Route::prefix('reviews')->name('review.')->middleware('auth')->group(function ()
     Route::post('/', [ReviewController::class, 'store'])->name('store');
     Route::put('/{review}', [ReviewController::class, 'update'])->name('update');
     Route::delete('/{review}', [ReviewController::class, 'destroy'])->name('delete');
+});
+
+Route::prefix('order')->name('order.')->middleware('auth')->group(function ()
+{
+    Route::get('/orders', [OrderController::class, 'index'])->name('index');
+    Route::get('/create', [OrderController::class, 'create'])->name('create');
+    Route::post('/', [OrderController::class, 'store'])->name('store');
+    Route::put('/{order}', [OrderController::class, 'update'])->name('update');
+    Route::delete('/{order}', [OrderController::class, 'destroy'])->name('delete');
 });
