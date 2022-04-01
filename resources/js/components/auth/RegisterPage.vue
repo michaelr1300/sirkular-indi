f25d2cac2942c40730902b841df15874be8da4b0
Michael Roberto <michaelr1300@gmail.com>
2022-04-01 14:03:39 +0700

    Login Register Page (WIP)

--- resources/js/components/auth/RegisterPage.vue
+++ resources/js/components/auth/RegisterPage.vue
@@ -4,13 +4,22 @@
       <div class="mx-auto">
         <img class="img-header-icon" src="/images/icon-indi-pair-color.svg" alt="logo-indi"/>
       </div>
-      <h4 class="fw-bold">MASUK</h4>
-      <div class="fw-bold">Masuk kembali ke akun Anda</div>
+      <h4 class="fw-bold">DAFTAR</h4>
+      <div class="fw-bold">Silakan daftar sebagai akun baru</div>
       <div class="mt-4">
-        <div v-if="hasError" class="text-danger">
-          Email atau password salah.
-        </div>
         <form @submit.prevent="doLogin">
+          <div class="mb-3">
+            <label for="name" class="form-label">Nama</label>
+            <input 
+              id="name" 
+              name="name" 
+              type="text" 
+              class="form-control" 
+              placeholder="Nama Anda"
+              required
+              v-model="form.name"
+            >
+          </div>
           <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input 
@@ -35,17 +44,17 @@
               v-model="form.password"
             >
           </div>
-
-          <div class="form-group">
-            <div class="custom-control custom-checkbox form-check-inline">
-                <input
-                  type="checkbox"
-                  class="custom-control-input"
-                  id="remember_me"
-                  v-model="form.remember"
-                />
-                <label for="remember_me" class="custom-control-label inline-flex items-center">Remember me</label>
-            </div>
+          <div class="mb-3">
+            <label for="password_confirmation" class="form-label">Konfirmasi Password</label>
+            <input 
+              id="password_confirmation" 
+              name="password_confirmation" 
+              type="password" 
+              class="form-control" 
+              placeholder="Konfirmasi password Anda"
+              required
+              v-model="form.password_confirmation"
+            >
           </div>
           <div class="mt-3">
             <button  
@@ -53,21 +62,18 @@
               class="btn btn-primary w-100"
               :disabled="isLoading"
             >
-              Login
+              Daftar
             </button>
           </div>
         </form>
         <p class="text-center text-muted mt-2">
-              Belum punya akun? Daftar
-              <span><a href="/register">di sini.</a></span>
-          </p>
-          <p class="text-center text-muted mt-2">
-              <span><a href="/password/reset">Lupa Password?</a></span>
-          </p>
+          Sudah punya akun? Masuk
+          <span><a href="/login">di sini.</a></span>
+        </p>
       </div>
     </div>
     <div class="col-12 col-sm-5 h-100">
-      <img class="img img-fluid" src="images/login-page.png" alt="img-login">
+      <img class="img img-fluid" src="images/register-page.png" alt="img-register">
     </div>
   </div>
 </template>
@@ -77,9 +83,10 @@ export default {
   data() {
     return {
       form: {
+        name: null,
         email: null,
         password: null,
-        remember: true,
+        password_confirmation: null,
       },
       hasError : false,
       isLoading: false,
@@ -92,11 +99,12 @@ export default {
       this.hasError = false;
 
       try {
-        let response = await axios.post("/login", this.form);
+        let response = await axios.post("/register", this.form);
         window.location = "/"
       } catch (error) {
         if (error.response.status == 422) {
           this.form.password = '';
+          this.form.password_confirmation = '';
           this.hasError = true;
         }
         this.errors = error.response.data.errors;
