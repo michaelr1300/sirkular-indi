b0a9ea338ffe4f39dfd28c09f60fb566eaaf3906
Michael Roberto <michaelr1300@gmail.com>
2022-04-19 16:39:18 +0700

    Order List (WIP)

--- resources/views/dashboard/order.blade.php
+++ resources/views/dashboard/order.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="container">
     DASHBOARD ORDER
-    <order-list :orders='{{ json_encode($orders) }}'></order-list>
+    <order-list :orders='{{ json_encode($orders) }}' :user='{{ json_encode(Auth::user()) }}'></order-list>
 </div>
 @endsection
